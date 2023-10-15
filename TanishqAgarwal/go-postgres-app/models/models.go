bbf3d6397427731a9f7b03f6ee1265ef02c05435
Adarsh Singh <adarshsingh1202@gmail.com>
2023-10-15 23:40:40 +0530

    ad

--- TanishqAgarwal/go-postgres-app/models/models.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// User schema of the user table
-type User struct {
-	ID       int64  `json:"id"`
-	Name     string `json:"name"`
-	Location string `json:"location"`
-	Age      int64  `json:"age"`
-}
--- oyejateen/src/apis/getAudioLink.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-/*const instance = axios.create({
-     
-});*/
-export default axios.create({
-  baseURL: "https://server-geet.iiiv.repl.co/",
-});
-//instance.defaults.headers.common['Authorization'] = "AIzaSyBh3IuBJGuNTYp70xeMSUpOBMmW2S5gFSA";
